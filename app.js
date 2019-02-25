7cf4a149d96d4fc62d11e5031dce1a6ac50660af
dustinur <dustinuriah@gmail.com>
2019-02-24 21:19:34 -0700

    crud complete

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+// localhost:3200/characters/test
+// localhost:3200/characters/create
 // app.js
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const character = require('./routes/character.route'); // Imports routes for the characters
+const character = require("./routes/character.route"); // Imports routes for the characters
 const app = express();
-app.use('/characters', character);
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+let dev_db_url =
+  "mongodb+srv://dustin:13251325@cluster0-dchnu.mongodb.net/cards?retryWrites=true";
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/characters", character);
 
 let port = 3200;
+
 app.listen(port, () => {
   console.log("Server is running at port# " + port);
 });
--- routes/character.route.js
+++ routes/character.route.js
@@ -6,4 +6,10 @@ const character_controller = require('../controllers/character.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', character_controller.test);
+
+router.post('/create', character_controller.character_create);
+router.get('/:id', character_controller.character_details);
+router.put('/:id/update', character_controller.character_update);
+router.delete('/:id/delete', character_controller.character_delete);
+
 module.exports = router;
